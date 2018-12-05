8fe76d510a8748675946325896492da008d4b841
Ian Page Hands <iphands@gmail.com>
2018-12-05 13:10:55 -0500

    add helper dev script

---
+++ scripts/dev_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+NAME=insgihts-proxy-test
+docker build -t $NAME .
+docker run --net=host -e PLATFORM=linux --rm -ti --name $NAME --security-opt label=disable -p 1337:1337 $NAME /bin/sh
