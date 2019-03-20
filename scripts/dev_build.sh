4882432f6a43ff1752bd01d5f50347f910025c30
Ian Page Hands <iphands@gmail.com>
2019-03-20 17:12:22 -0400

    revert debug on dev launch

--- scripts/dev_build.sh
+++ scripts/dev_build.sh
@@ -3,4 +3,5 @@ set -e
 
 NAME=insights-proxy-test
 docker build -t $NAME .
-docker run --net=host -e PLATFORM=linux --rm -ti --name $NAME --security-opt label=disable -p 1337:1337 $NAME /bin/sh
+clear
+docker run --net=host -e PLATFORM=linux --rm -ti --name $NAME --security-opt label=disable $NAME
